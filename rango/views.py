6b32269be295e56e00847f8eb28bb673af55c1c0
Marc Peucker <marc.peucker@gmail.com>
2016-12-05 16:54:32 +0100

    Added another url(About) and link back to main site

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    html = "Rango says hey there partner!" + '<br/><a href="/rango/about/">About</a>'
+    return HttpResponse(html)
+
+def about(request):
+    html = "Rango say here is the about page." + '<br/><a href="/rango/">Main Site</a>'
+    return HttpResponse(html)
